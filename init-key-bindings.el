4fa9b8c811d552e82159405ce57b84850ecb36b4
spaceblock <spaceblock@gmail.com>
2015-04-02 23:46:45 +0300

    first

---
+++ init-key-bindings.el
@@ -0,0 +1,13 @@
+;; init-key-bindings.el
+;;----------------------------------------------------------------------------
+;; Key bindings
+;;----------------------------------------------------------------------------
+
+;(global-set-key (kbd "C-x /") 'shell-command) ;; run shell
+; или вообще применить клавишу super как meta
+(setq x-super-keysym 'meta) ;; set meta key
+(setq meta 'ctrl) ;; set ctrl - alt
+
+
+(provide 'init-key-bindings)
+
---
+++ init.el
@@ -0,0 +1,21 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(show-paren-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(load "~/.emacs.d/init-key-bindings.el")
